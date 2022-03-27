9387a937205d2187d2500d5c179e01b2aa0e834b
myrwynnepascual <myrwynne.pascual.iics@ust.edu.ph>
2022-03-28 00:25:04 +0800

    with game mechanics

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ function App() {
   var current_score = 0;
   var final_score = score_label + current_score;
 
+  var round_label = 'Round: ';
+  var round_count = 0;
+  var round = round_label +round_count;
+
   var random_card1 = Math.floor(Math.random() * 13 ) + 1;
   var random_card2 = Math.floor(Math.random() * 13 ) + 1;
   var random_card3 = Math.floor(Math.random() * 13 ) + 1;
@@ -32,7 +36,29 @@ function App() {
       <div className='in-between'>
 
         <h1>In between</h1>
-        <h2>{final_score}</h2>
+
+        <h2>Game Mechanics</h2>
+        <p>
+          <li>The game has 5 rounds only.</li>
+          <li>You will be given 2 random cards each round.</li>
+          <li>Guess if the 3rd card is between the numbers in card 1 and card 2.</li>
+          <li>You have an option to choose between <strong>DEAL</strong> or <strong>NO DEAL</strong>.</li>
+          
+          <br></br>
+
+          <p>If the 2 numbers in the card are <strong>NOT identical</strong>:</p>
+          <li>If you chose "Deal" and the 3rd card is in between the 2 numbers in the cards, you will get 1 point. Otherwise, you lose 1 point.</li>
+          <li>If you chose "No Deal" and the 3rd card is in between the 2 numbers in the cards, you will lose 0.5 point.</li>
+
+          <br></br>
+
+          <p>If the 2 numbers in the card are <strong>IDENTICAL</strong>:</p>
+          <li>You have an option to choose between <strong>HIGHER</strong> or <strong>LOWER</strong>.</li>
+          <li>If you chose "Higher" and the 3rd card is higher than the number in the first 2 cards, you will get 1 point. Otherwise, you lose 1 point.</li>
+          <li>If you chose "Lower" and the 3rd card is lower than the number in the first 2 cards, you will get 1 point. Otherwise, you lose 1 point. </li>
+        </p>
+
+        <h3>{round} <br></br> {final_score} </h3>
 
         <div className='row1'>
 
